4694d87d539855f7528678a81fa82971cac0b5b5
Myles Wright <mbwright@butler.edu>
2020-03-26 17:05:09 -0400

    Added buttons to userviewform

--- unnamedProject/unnamedProject/Forms/UserViewForm.Designer.cs
+++ unnamedProject/unnamedProject/Forms/UserViewForm.Designer.cs
@@ -29,11 +29,131 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserViewForm));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.minimizeBtn = new System.Windows.Forms.Button();
+            this.fullBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.backButtonList = new System.Windows.Forms.ImageList(this.components);
+            this.back = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.SystemColors.WindowFrame;
+            this.imageList1.Images.SetKeyName(0, "588a6507d06f6719692a2d15.png");
+            this.imageList1.Images.SetKeyName(1, "icons8-restore-down-52.png");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "icons8-restore-down-52.png");
+            // 
+            // minimizeBtn
+            // 
+            this.minimizeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minimizeBtn.FlatAppearance.BorderSize = 0;
+            this.minimizeBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.minimizeBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.minimizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimizeBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.minimizeBtn.Location = new System.Drawing.Point(695, 7);
+            this.minimizeBtn.Name = "minimizeBtn";
+            this.minimizeBtn.Size = new System.Drawing.Size(24, 32);
+            this.minimizeBtn.TabIndex = 21;
+            this.minimizeBtn.Text = "-";
+            this.minimizeBtn.UseVisualStyleBackColor = true;
+            // 
+            // fullBtn
+            // 
+            this.fullBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fullBtn.FlatAppearance.BorderSize = 2;
+            this.fullBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.fullBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.fullBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.fullBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.fullBtn.ImageList = this.imageList2;
+            this.fullBtn.Location = new System.Drawing.Point(725, 18);
+            this.fullBtn.Name = "fullBtn";
+            this.fullBtn.Size = new System.Drawing.Size(20, 12);
+            this.fullBtn.TabIndex = 20;
+            this.fullBtn.UseVisualStyleBackColor = true;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitBtn.FlatAppearance.BorderSize = 0;
+            this.exitBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.exitBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.exitBtn.Location = new System.Drawing.Point(748, 9);
+            this.exitBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(43, 32);
+            this.exitBtn.TabIndex = 19;
+            this.exitBtn.Text = "X";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            // 
+            // backButtonList
+            // 
+            this.backButtonList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("backButtonList.ImageStream")));
+            this.backButtonList.TransparentColor = System.Drawing.Color.Transparent;
+            this.backButtonList.Images.SetKeyName(0, "icons8-rewind-button-round-80.png");
+            this.backButtonList.Images.SetKeyName(1, "backbutton.png");
+            // 
+            // back
+            // 
+            this.back.AutoSize = true;
+            this.back.BackColor = System.Drawing.Color.Transparent;
+            this.back.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.back.FlatAppearance.BorderSize = 0;
+            this.back.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.back.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.back.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.back.ImageIndex = 1;
+            this.back.ImageList = this.backButtonList;
+            this.back.Location = new System.Drawing.Point(13, 13);
+            this.back.Margin = new System.Windows.Forms.Padding(4);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(48, 48);
+            this.back.TabIndex = 22;
+            this.back.UseVisualStyleBackColor = false;
+            // 
+            // UserViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.minimizeBtn);
+            this.Controls.Add(this.fullBtn);
+            this.Controls.Add(this.exitBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "UserViewForm";
             this.Text = "UserViewForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button minimizeBtn;
+        private System.Windows.Forms.Button fullBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.ImageList backButtonList;
+        private System.Windows.Forms.Button back;
     }
 }
