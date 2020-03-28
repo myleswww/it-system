8b9314eb0376dfda460f04bacc9dc01b06a5db96
Myles Wright <mbwright@butler.edu>
2020-03-28 18:23:19 -0400

    Created GUI for settings page

--- unnamedProject/unnamedProject/Forms/SettingsForm.Designer.cs
+++ unnamedProject/unnamedProject/Forms/SettingsForm.Designer.cs
@@ -29,11 +29,239 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backButtonList = new System.Windows.Forms.ImageList(this.components);
+            this.back = new System.Windows.Forms.Button();
+            this.minimizeBtn = new System.Windows.Forms.Button();
+            this.fullBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.BtnEdit = new System.Windows.Forms.Button();
+            this.LblEmail = new System.Windows.Forms.Label();
+            this.LblUsername = new System.Windows.Forms.Label();
+            this.LblPassword = new System.Windows.Forms.Label();
+            this.TxtbxEmail = new System.Windows.Forms.TextBox();
+            this.TxtbxUsername = new System.Windows.Forms.TextBox();
+            this.TxtbxPassword = new System.Windows.Forms.TextBox();
+            this.LblSettings = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.SystemColors.WindowFrame;
+            this.imageList1.Images.SetKeyName(0, "588a6507d06f6719692a2d15.png");
+            this.imageList1.Images.SetKeyName(1, "icons8-restore-down-52.png");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "icons8-restore-down-52.png");
+            // 
+            // backButtonList
+            // 
+            this.backButtonList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("backButtonList.ImageStream")));
+            this.backButtonList.TransparentColor = System.Drawing.Color.Transparent;
+            this.backButtonList.Images.SetKeyName(0, "icons8-rewind-button-round-80.png");
+            this.backButtonList.Images.SetKeyName(1, "backbutton.png");
+            // 
+            // back
+            // 
+            this.back.AutoSize = true;
+            this.back.BackColor = System.Drawing.Color.Transparent;
+            this.back.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.back.FlatAppearance.BorderSize = 0;
+            this.back.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.back.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.back.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.back.ImageIndex = 1;
+            this.back.ImageList = this.backButtonList;
+            this.back.Location = new System.Drawing.Point(2, 1);
+            this.back.Margin = new System.Windows.Forms.Padding(4);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(48, 48);
+            this.back.TabIndex = 23;
+            this.back.UseVisualStyleBackColor = false;
+            // 
+            // minimizeBtn
+            // 
+            this.minimizeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minimizeBtn.FlatAppearance.BorderSize = 0;
+            this.minimizeBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.minimizeBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.minimizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimizeBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.minimizeBtn.Location = new System.Drawing.Point(707, 8);
+            this.minimizeBtn.Name = "minimizeBtn";
+            this.minimizeBtn.Size = new System.Drawing.Size(21, 30);
+            this.minimizeBtn.TabIndex = 26;
+            this.minimizeBtn.Text = "-";
+            this.minimizeBtn.UseVisualStyleBackColor = true;
+            // 
+            // fullBtn
+            // 
+            this.fullBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fullBtn.FlatAppearance.BorderSize = 2;
+            this.fullBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.fullBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.fullBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.fullBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.fullBtn.ImageList = this.imageList2;
+            this.fullBtn.Location = new System.Drawing.Point(733, 18);
+            this.fullBtn.Name = "fullBtn";
+            this.fullBtn.Size = new System.Drawing.Size(18, 11);
+            this.fullBtn.TabIndex = 25;
+            this.fullBtn.UseVisualStyleBackColor = true;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitBtn.FlatAppearance.BorderSize = 0;
+            this.exitBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.exitBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.exitBtn.Location = new System.Drawing.Point(753, 9);
+            this.exitBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(38, 30);
+            this.exitBtn.TabIndex = 24;
+            this.exitBtn.Text = "X";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            // 
+            // BtnEdit
+            // 
+            this.BtnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnEdit.Font = new System.Drawing.Font("Britannic Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEdit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BtnEdit.Location = new System.Drawing.Point(373, 294);
+            this.BtnEdit.Name = "BtnEdit";
+            this.BtnEdit.Size = new System.Drawing.Size(75, 25);
+            this.BtnEdit.TabIndex = 27;
+            this.BtnEdit.Text = "Edit";
+            this.BtnEdit.UseVisualStyleBackColor = true;
+            // 
+            // LblEmail
+            // 
+            this.LblEmail.AutoSize = true;
+            this.LblEmail.Font = new System.Drawing.Font("Britannic Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblEmail.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblEmail.Location = new System.Drawing.Point(305, 158);
+            this.LblEmail.Name = "LblEmail";
+            this.LblEmail.Size = new System.Drawing.Size(45, 16);
+            this.LblEmail.TabIndex = 28;
+            this.LblEmail.Text = "Email:";
+            // 
+            // LblUsername
+            // 
+            this.LblUsername.AutoSize = true;
+            this.LblUsername.Font = new System.Drawing.Font("Britannic Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblUsername.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblUsername.Location = new System.Drawing.Point(274, 202);
+            this.LblUsername.Name = "LblUsername";
+            this.LblUsername.Size = new System.Drawing.Size(73, 16);
+            this.LblUsername.TabIndex = 29;
+            this.LblUsername.Text = "Username:";
+            // 
+            // LblPassword
+            // 
+            this.LblPassword.AutoSize = true;
+            this.LblPassword.Font = new System.Drawing.Font("Britannic Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPassword.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblPassword.Location = new System.Drawing.Point(278, 253);
+            this.LblPassword.Name = "LblPassword";
+            this.LblPassword.Size = new System.Drawing.Size(72, 16);
+            this.LblPassword.TabIndex = 30;
+            this.LblPassword.Text = "Password:";
+            // 
+            // TxtbxEmail
+            // 
+            this.TxtbxEmail.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.TxtbxEmail.Font = new System.Drawing.Font("Britannic Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtbxEmail.Location = new System.Drawing.Point(357, 153);
+            this.TxtbxEmail.Name = "TxtbxEmail";
+            this.TxtbxEmail.Size = new System.Drawing.Size(100, 22);
+            this.TxtbxEmail.TabIndex = 31;
+            // 
+            // TxtbxUsername
+            // 
+            this.TxtbxUsername.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.TxtbxUsername.Font = new System.Drawing.Font("Britannic Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtbxUsername.Location = new System.Drawing.Point(357, 199);
+            this.TxtbxUsername.Name = "TxtbxUsername";
+            this.TxtbxUsername.Size = new System.Drawing.Size(100, 22);
+            this.TxtbxUsername.TabIndex = 32;
+            // 
+            // TxtbxPassword
+            // 
+            this.TxtbxPassword.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.TxtbxPassword.Font = new System.Drawing.Font("Britannic Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtbxPassword.Location = new System.Drawing.Point(357, 248);
+            this.TxtbxPassword.Name = "TxtbxPassword";
+            this.TxtbxPassword.Size = new System.Drawing.Size(100, 22);
+            this.TxtbxPassword.TabIndex = 33;
+            // 
+            // LblSettings
+            // 
+            this.LblSettings.AutoSize = true;
+            this.LblSettings.Font = new System.Drawing.Font("Britannic Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSettings.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LblSettings.Location = new System.Drawing.Point(352, 67);
+            this.LblSettings.Name = "LblSettings";
+            this.LblSettings.Size = new System.Drawing.Size(98, 27);
+            this.LblSettings.TabIndex = 34;
+            this.LblSettings.Text = "Settings";
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblSettings);
+            this.Controls.Add(this.TxtbxPassword);
+            this.Controls.Add(this.TxtbxUsername);
+            this.Controls.Add(this.TxtbxEmail);
+            this.Controls.Add(this.LblPassword);
+            this.Controls.Add(this.LblUsername);
+            this.Controls.Add(this.LblEmail);
+            this.Controls.Add(this.BtnEdit);
+            this.Controls.Add(this.minimizeBtn);
+            this.Controls.Add(this.fullBtn);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.back);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SettingsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SettingsForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ImageList backButtonList;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button minimizeBtn;
+        private System.Windows.Forms.Button fullBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button BtnEdit;
+        private System.Windows.Forms.Label LblEmail;
+        private System.Windows.Forms.Label LblUsername;
+        private System.Windows.Forms.Label LblPassword;
+        private System.Windows.Forms.TextBox TxtbxEmail;
+        private System.Windows.Forms.TextBox TxtbxUsername;
+        private System.Windows.Forms.TextBox TxtbxPassword;
+        private System.Windows.Forms.Label LblSettings;
     }
 }
